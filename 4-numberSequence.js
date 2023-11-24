075107b6be01c31003033074c35c986f99899143
karadagserhat <serhatkaradagg@outlook.com>
2023-11-24 18:11:47 +0300

    fixed problems

--- 4-numberSequence.js
+++ 4-numberSequence.js
@@ -1,6 +1,12 @@
+// count sequences, three numbers, first equals to third
+// and middle one different, all three numbers equal does not count [4,4,4]
+
 const first = [8, 6, 8, 6, 7, 4, 7];
+// [8,6,8], [6,8,6], [7,4,7];
 const second = [4, 5, 5, 7, 5, 5, 9];
+// [5,7,6]
 const third = [3, 3, 2, 8, 7, 6, 5];
+//
 
 function count(data) {
   return data.reduce((total, item, i, arr) => {
---
+++ a.js
@@ -0,0 +1,30 @@
+/************* */
+// 1- return3Times
+//console.log(example(1, 2)(3, 4)(5, 6));
+
+/**************** */
+// 2- multiplyArguments
+// console.log(multiply(4, 5)); 4*5
+// console.log(multiply(2, 0, 4, 2)); 2 + 0 * 4 * 2
+
+/************** */
+// 3- daysBetween
+// const date1 = new Date('May 27, 2021');
+// const date2 = new Date('June 5, 2019');
+// console.log(getDays(date1, date2));
+
+/************ */
+// 4- numberSequence
+// count sequences, three numbers, first equals to third
+// and middle one different, all three numbers equal does not count [4,4,4]
+//const first = [8, 6, 8, 6, 7, 4, 7]
+// [8,6,8], [6,8,6], [7,4,7];
+//const second = [4, 5, 5, 7, 5, 5, 9]
+// [5,7,5]
+//const third = [3, 3, 2, 8, 7, 6, 5]
+// 0
+// console.log(count(first));
+
+/*************** */
+// 5- evenOrOdd
+// console.log(sum(112)); 1+ 1+ 2 -> "EVEN"
