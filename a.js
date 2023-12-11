d86054fbeed50846ce5e1a44600e32d166cd2034
karadagserhat <serhatkaradagg@outlook.com>
2023-12-11 17:11:21 +0300

    fixed problems

--- a.js
+++ a.js
@@ -99,3 +99,19 @@
 /************** */
 // findSecondLargest
 // console.log(findSecondLargest([5, 2, 10, 8, 3]));
+
+function findSecondLargest(arr) {
+  let largest = arr[0];
+  let secondLargest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+console.log(findSecondLargest([5, 2, 10, 8, 3]));
